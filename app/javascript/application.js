23f7dc4b1650e0205aaad5cbd47ce178544ab36e
Yunjidev <florian.vancamp@gmail.com>
2023-08-29 16:16:16 +0200

    Add Landing Pages + Navbar Tailwind

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+  const dropdownToggle = document.getElementById('auth-dropdown-toggle');
+  const dropdownMenu = document.getElementById('auth-dropdown');
+
+  dropdownToggle.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('hidden');
+  });
+
+  // Close dropdown when clicking outside
+  window.addEventListener('click', (event) => {
+    if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+      dropdownMenu.classList.add('hidden');
+    }
+  });
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages#index', to: 'static_pages#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  root "static_pages#index"
 end
