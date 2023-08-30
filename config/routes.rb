a531112c043b95ec1536157507b55ba584007590
Yunjidev <florian.vancamp@gmail.com>
2023-08-30 11:00:51 +0200

    Ajout de la page index des produits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'items/index'
   get 'static_pages#index', to: 'static_pages#index'
   devise_for :users
+
+  resources :items, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
