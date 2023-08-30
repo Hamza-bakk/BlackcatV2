816e0ffb15a4b2d98c2174d6fda7fb996f632ae6
Yunjidev <florian.vancamp@gmail.com>
2023-08-30 16:09:51 +0200

    Ajout du lien Dashboard accessible que par les users admin connecté

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'items/index'
   get 'static_pages#index', to: 'static_pages#index'
   get 'contact', to: 'contact#index'
+  get 'dashboard', to: 'dashboard#index'
   devise_for :users, path: '', path_names: { sign_in: 'connexion', sign_out: 'deconnexion', sign_up: 'inscription' }
 
   resources :items, only: [:index, :show], path: 'produits'
