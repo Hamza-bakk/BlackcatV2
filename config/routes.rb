2111c10734fd71d39cbae1641295a31c49887abd
Yunjidev <florian.vancamp@gmail.com>
2023-08-30 17:33:52 +0200

    Ajout du profil utilisateur + Edit profil et mdp

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'connexion', sign_out: 'deconnexion', sign_up: 'inscription' }
 
   resources :items, only: [:index, :show], path: 'produits'
+  resource :profile, only: [:show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
