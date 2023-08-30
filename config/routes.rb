216116be4b44fb9de46dd210835838c1a24fdd8e
Yunjidev <florian.vancamp@gmail.com>
2023-08-30 11:55:00 +0200

    Ajout formulaire de contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'items/index'
   get 'static_pages#index', to: 'static_pages#index'
+  get 'contact', to: 'contact#index'
   devise_for :users
 
   resources :items, only: [:index, :show]
