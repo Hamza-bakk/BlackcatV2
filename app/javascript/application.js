c77154fad6fe360541b603d0289001a7742d8ea3
Yunjidev <florian.vancamp@gmail.com>
2023-08-31 11:42:37 +0200

    Ajout meta + reglage seo ect

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,16 +2,12 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-  const dropdownToggle = document.getElementById('auth-dropdown-toggle');
-  const dropdownMenu = document.getElementById('auth-dropdown');
 
-  dropdownToggle.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('hidden');
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const mobileNav = document.querySelector(".hidden.md\\:flex");
 
-  // Close dropdown when clicking outside
-  window.addEventListener('click', (event) => {
-    if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
-      dropdownMenu.classList.add('hidden');
-    }
+  hamburgerButton.addEventListener("click", function () {
+    mobileNav.classList.toggle("hidden"); // Toggle la classe "hidden" pour afficher ou masquer le menu
   });
+});
